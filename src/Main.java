1108bf4698bd76ab313d851173cf77d6be1340cb
xavierathlan <xavierathlan@hotmail.com>
2022-03-11 21:05:18 -0300

    Melhorando a qualidade do código

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso java");
+        curso1.setDescricao("descrição curso java");
+        curso1.setCargahoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso php");
+        curso2.setDescricao("descrição curso php");
+        curso2.setCargahoraria(5);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentori de java");
+        mentoria.setDescricao("descrição mentoria java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
